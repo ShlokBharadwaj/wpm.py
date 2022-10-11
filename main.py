eb5bb1672c8afc14ff1743c1d82d94bf66737671
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-12 00:00:22 +0530

    Update: Text color and background color

--- main.py
+++ main.py
@@ -2,25 +2,15 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.Color.RED, curses.Color.BLACK)
+    curses.init_pair(2, curses.Color.YELLOW, curses.Color.BLACK)
+    RED_AND_BLACK = curses.color_pair(1)
+    YELLOW_AND_BLACK = curses.color_pair(2)
+
+
     stdscr.clear()
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_ALTCHARSET)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_BLINK)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_BOLD)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_DIM)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_INVIS)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_ITALIC)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_NORMAL)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_PROTECT)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_REVERSE)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_STANDOUT)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_UNDERLINE)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_HORIZONTAL)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_LEFT)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_LOW)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_RIGHT)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_TOP)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_VERTICAL)
-    # stdscr.addstr(1, 1, "Hello World!", curses.A_CHARTEXT)
+    stdscr.addstr(0, 0, "Hello World!", RED_AND_BLACK)
+    stdscr.addstr(10, 10, "Hello World!", YELLOW_AND_BLACK)
     stdscr.refresh()
     stdscr.getkey()
 
