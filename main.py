88daf12bac403204f3e5bf90d1613e18f3347a8a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-12 16:16:55 +0530

    Update: Add New Colors and Remove pad

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import curses
 from curses import wrapper
+from curses.textpad import Textbox, rectangle
 import time
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    RED_AND_BLACK = curses.color_pair(1)
-    YELLOW_AND_BLACK = curses.color_pair(2)
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    WHITE_AND_BLACK = curses.color_pair(1)
+    GREEN_AND_BLACK = curses.color_pair(2)
+    RED_AND_BLACK = curses.color_pair(3)
+    YELLOW_AND_BLACK = curses.color_pair(4)
 
-    pad = curses.newpad(100, 100)
-    # curses.LINES - 1, curses.COLS - 1 
+    stdscr.clear()
     stdscr.refresh()
+    stdscr.nodelay(True)
+    stdscr.timeout(1000)
+    stdscr.getch()
 
-    for i in range(100):
-        for j in range(26):
-            char = chr(67 + j)
-            pad.addstr(char, YELLOW_AND_BLACK)
-
-    for i in range(100):
-        stdscr.clear()
-        stdscr.refresh()
-        pad.refresh(i, 0, 0, 0, 20, 20)
-        time.sleep(0.1)
-    stdscr.getkey()
 
 wrapper(main)
