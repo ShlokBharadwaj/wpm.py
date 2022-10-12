bfd2b6aa6b477aea6fc0b4cf23a68e4774ca125f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-13 05:13:43 +0530

    Add: Rectangle to display the text

--- main.py
+++ main.py
@@ -12,30 +12,9 @@ def main(stdscr):
     GREEN_AND_BLACK = curses.color_pair(2)
     RED_AND_BLACK = curses.color_pair(3)
     YELLOW_AND_BLACK = curses.color_pair(4)
-    stdscr.nodelay(True)
     
-    x, y = 0, 0
-    string_x = 0
-    while True: 
-        try:
-            key = stdscr.getkey()
-        except:
-            key = None
-        if key == 'KEY_UP': 
-            y -= 1
-        elif key == 'KEY_DOWN':
-            y += 1
-        elif key == 'KEY_LEFT':
-            x -= 1
-        elif key == 'KEY_RIGHT':
-            x += 1
-
-        stdscr.clear()
-        string_x += 1
-        stdscr.addstr(0, string_x//100, "Hello World!")
-
-        stdscr.addstr(y, x, "0")
-        stdscr.refresh()
-
+    rectangle(stdscr, 2, 2, 10, 40)
+    stdscr.refresh()
+    stdscr.getch()
 
 wrapper(main)
