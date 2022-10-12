d629b6d8ee37db797b5cb4caa24bc6fef07057cc
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-12 06:26:49 +0530

    Update: Have static string display alongside window

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main(stdscr):
     RED_AND_BLACK = curses.color_pair(1)
     YELLOW_AND_BLACK = curses.color_pair(2)
 
+    counter_window = curses.newwin(1, 20, 10, 10)
+    stdscr.addstr("Hello World!")
+    stdscr.refresh()
 
     for i in range(100):
-        stdscr.clear()
+        counter_window.clear()
         color = RED_AND_BLACK if i % 2 == 0 else YELLOW_AND_BLACK
-        stdscr.addstr(f"Count: {i}", color)
-        stdscr.refresh()
+        counter_window.addstr(f"Count: {i}", color)
+        counter_window.refresh()
         time.sleep(0.1)
     stdscr.getkey()
 
