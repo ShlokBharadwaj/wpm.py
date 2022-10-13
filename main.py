683dd4748ac9737b86308ccfc212dfa17ffeff44
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-13 15:13:56 +0530

    Update: Append and delete characters with backspace

--- main.py
+++ main.py
@@ -40,7 +40,11 @@ def wpm_test(stdscr):
         if ord(key) == 27:
             break
 
-        current_text.append(key)
+        if key in('KEY_BACKSPACE', '\b', '\x7f'):
+            if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
 
 
 def main(stdscr):
