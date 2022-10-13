186de21f2fb18bc2529ef3fac11740af52b5e878
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-13 06:20:17 +0530

    Add: Color pairs and border

--- main.py
+++ main.py
@@ -7,30 +7,25 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_GREEN, curses.COLOR_YELLOW)
+    curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_WHITE)
     WHITE_AND_BLACK = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
     RED_AND_BLACK = curses.color_pair(3)
-    YELLOW_AND_BLACK = curses.color_pair(4)
+    MAGNETA_AND_BLACK = curses.color_pair(4)
+    CYAN_AND_BLACK = curses.color_pair(5)
+    BLUE_AND_BLACK = curses.color_pair(6)
+    GREEN_AND_YELLOW = curses.color_pair(7)
+    BLACK_AND_WHITE = curses.color_pair(8)
     curses.echo()
 
-    stdscr.attron(RED_AND_BLACK)
+    stdscr.attron(MAGNETA_AND_BLACK)
     stdscr.border()
-    stdscr.attroff(RED_AND_BLACK)
-
-    stdscr.attron(GREEN_AND_BLACK)
-    rectangle(stdscr, 1, 1, 10, 10)
-    stdscr.addstr(5, 30, "Hello World!")
-    stdscr.attroff(GREEN_AND_BLACK)
-
-    
-    stdscr.move(30, 10)
+    stdscr.attroff(MAGNETA_AND_BLACK)
     
     stdscr.refresh()
-
-    while True:
-        key = stdscr.getkey()
-        if key == 'q':
-            break
-
+    stdscr.getkey()
 wrapper(main)
